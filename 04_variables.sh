d3b742ad712f6de644a954d14fa73295e42f4529
harshateja9597 <harshateja9597@gmail.com>
2024-04-26 12:43:45 +0530

    adding colors

--- 04_variables.sh
+++ 04_variables.sh
@@ -25,18 +25,18 @@ USER=$(id -u)
 
 if [ $USER -ne 0 ] 
 then
-    echo "you are not a root user"
+    echo -e " $R you are not a root user $N"
     exit 1
     else
-    echo "you are a root user"  
+    echo " $G you are a root user $N"  
 fi
 
 yum install mysql -y >>& $LOGFILE
  
-validate() $? "installing myql"
+validate $? "installing myql"
 
 yum install git -y >>& $LOGFILE
 
-validate() $? "installing git"
+validate $? "installing git"
 
 
